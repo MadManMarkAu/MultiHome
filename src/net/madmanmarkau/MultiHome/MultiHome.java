9a3f76c6bba031b8411100147dc67a87ebf47620
MadManMarkAu <madmanmarkau@gmail.com>
2011-04-10 19:36:04 +0800

    Added new configuration for MaxHomes, WarmUp and CoolDown. v0.6

--- src/net/madmanmarkau/MultiHome/MultiHome.java
+++ src/net/madmanmarkau/MultiHome/MultiHome.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
 import java.util.logging.Logger;
 
@@ -17,6 +18,7 @@ import org.bukkit.Location;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -27,6 +29,7 @@ import com.nijikokun.bukkit.Permissions.Permissions;
 public class MultiHome extends JavaPlugin {
 	public final Logger log = Logger.getLogger("Minecraft");
     public PermissionHandler Permissions;
+	public Configuration Config;
 	public PluginDescriptionFile pdfFile;
 	public PluginLogger pluginLogger; 
 
@@ -45,8 +48,9 @@ public class MultiHome extends JavaPlugin {
 		this.homesPath = this.getDataFolder().getAbsolutePath() + File.separator;
 
 		setupHelp();
-		loadHomes();
 		setupPermissions();
+		loadSettings();
+		loadHomes();
 
 		log.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " loaded");
 	}
@@ -73,6 +77,55 @@ public class MultiHome extends JavaPlugin {
         }
     }
 
+    private void loadSettings() {
+		File file = new File(this.homesPath);
+		if ( !(file.exists()) ) {
+			file.mkdir();
+		}
+
+		// Create configuration file if not exist
+		file = new File(this.homesPath + "/config.yml");
+		if ( !file.exists() ) {
+			try {
+				FileWriter fstream = new FileWriter(this.homesPath + "/config.yml");
+				BufferedWriter out = new BufferedWriter(fstream);
+
+				String newline = System.getProperty("line.separator");
+				
+				out.write("# MultiHome config file." + newline);
+				out.write("#" + newline);
+				out.write("# settings:" + newline);
+				out.write("#   deafult: Default settings for all users are stored here." + newline);
+				out.write("#     warmup: Amount of time to wait before a /home command executes." + newline);
+				out.write("#     cooldown: Amount of time to wait before /home can be used again." + newline);
+				out.write("#     maxhomes: Maximum number of homes this group may have. Use -1 to signify no limit." + newline);
+				out.write("#" + newline);
+				out.write("# When editing this file for the first time, please duplicate the groups.default section" + newline);
+				out.write("#  for each of your defined Permissions groups." + newline);
+				out.write(newline);
+				out.write("MultiHome:" + newline);
+				out.write("    default:" + newline);
+				out.write("        warmup: 0" + newline);
+				out.write("        cooldown: 0" + newline);
+				out.write("        maxhomes: -1" + newline);
+				out.write("    groups:" + newline);
+				out.write("        default:" + newline);
+				out.write("            warmup: 0" + newline);
+				out.write("            cooldown: 0" + newline);
+				out.write("            maxhomes: -1" + newline);
+
+				out.close();
+			} catch (Exception e) {
+				log.warning(pdfFile.getName() + " could not write the default config file.");
+				this.getServer().getPluginManager().disablePlugin(this);
+			}
+		}
+
+    	// Reading from YML file
+		Config = new Configuration(new File(this.homesPath + "/config.yml"));
+		Config.load();
+    }
+    
 	public void setupPermissions() {
 		Plugin perm = this.getServer().getPluginManager().getPlugin("Permissions");
 			
@@ -291,6 +344,36 @@ public class MultiHome extends JavaPlugin {
 		return false;
 	}
 
+	public boolean queryPlayerHomeLocationExists(Player player, String name) {
+		ArrayList<HomeLocation> thisLocationList;
+
+		if (!homeLocations.containsKey(player.getName())) {
+			thisLocationList = new ArrayList<HomeLocation>();
+		} else {
+			thisLocationList = homeLocations.get(player.getName());
+		}
+
+		for (HomeLocation thisLoc : thisLocationList) {
+			if (thisLoc.getHomeName().compareToIgnoreCase(name) == 0) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public int getPlayerHomeCount(Player player) {
+		ArrayList<HomeLocation> thisLocationList;
+
+		if (!homeLocations.containsKey(player.getName())) {
+			thisLocationList = new ArrayList<HomeLocation>();
+		} else {
+			thisLocationList = homeLocations.get(player.getName());
+		}
+
+		return thisLocationList.size();
+	}
+	
 	public ArrayList<HomeLocation> listPlayerHomeLocations(String player) {
 		ArrayList<HomeLocation> thisLocationList;
 
@@ -303,6 +386,24 @@ public class MultiHome extends JavaPlugin {
 		return thisLocationList;
 	}
 
+	private int getSetting(Player player, String setting, int defaultValue) {
+		// Get the player group
+		String playerGroup = Permissions.getGroup(player.getWorld().getName(), player.getName());
+		
+		if (playerGroup != null) {
+			// Player group found
+			List<String> keys = Config.getKeys("MultiHome." + playerGroup);
+			
+			if (keys != null && !keys.isEmpty()) {
+				// Settings for player group exists.
+				return Config.getInt("MultiHome.groups." + playerGroup + "." + setting, defaultValue);
+			}
+		}
+		
+		// Get from default
+		return Config.getInt("MultiHome.default." + setting, defaultValue);
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
 	{
@@ -326,26 +427,49 @@ public class MultiHome extends JavaPlugin {
 			if (loc == null) {
 				player.sendMessage(ChatColor.RED + "Home not set.");
 			} else {
-				if (loc.getWorld().getName().equals(player.getWorld().getName())) {
-					// Direct teleport inside the current world.
-					player.teleportTo(loc);
-				} else {
-					// Indirect teleport between worlds.
-					Location playerLoc = player.getLocation();
+				if (HomeCoolDown.userMayTeleport(player)) {
+					HomeCoolDown.addCooldown(player.getName(), getSetting(player, "cooldown", 0));
+					int warmup = getSetting(player, "warmup", 0);
 					
-					player.teleportTo(new Location(loc.getWorld(), playerLoc.getX(), playerLoc.getY(), playerLoc.getZ(), playerLoc.getPitch(), playerLoc.getYaw()));
-					player.teleportTo(loc);
+					if (warmup >= 0) {
+						// Warpup required.
+						if (getServer().getScheduler().scheduleSyncDelayedTask(this, new HomeWarmUp(player, loc), warmup * 20) != -1) {
+							player.sendMessage(ChatColor.RED + "Home initiated. Transfer in " + warmup + " seconds.");
+						} else {
+							player.sendMessage(ChatColor.RED + "Home initiation FAILED! Please try again.");
+						}
+					} else {
+						// No warmup needed. Immediately teleport.
+						if (loc.getWorld().getName().equals(player.getWorld().getName())) {
+							// Direct teleport inside the current world.
+							player.teleportTo(loc);
+						} else {
+							// Indirect teleport between worlds.
+							Location playerLoc = player.getLocation();
+							
+							player.teleportTo(new Location(loc.getWorld(), playerLoc.getX(), playerLoc.getY(), playerLoc.getZ(), playerLoc.getPitch(), playerLoc.getYaw()));
+							player.teleportTo(loc);
+						}
+					}
 				}
 			}
 			return true;
 		} else if (cmd.getName().compareToIgnoreCase("multihome_sethome") == 0) {
 			if (args.length > 0) {
-				if (!this.Permissions.has(player, "multihome.namedhome")) return true;
-				setPlayerHomeLocation(player, args[0], player.getLocation());
-				player.sendMessage(ChatColor.RED + "Home location [" + args[0] + "] set.");
-				log.info(player.getName() + " set home location [" + args[0] +"].");
+				if (!this.Permissions.has(player, "multihome.setnamedhome")) return true;
+				int numHomes = getPlayerHomeCount(player);
+				int maxHomes = getSetting(player, "maxhomes", -1);
+				
+				if (numHomes < maxHomes || maxHomes == -1 || queryPlayerHomeLocationExists(player, args[0])) {
+					setPlayerHomeLocation(player, args[0], player.getLocation());
+					player.sendMessage(ChatColor.RED + "Home location [" + args[0] + "] set.");
+					log.info(player.getName() + " set home location [" + args[0] +"].");
+				} else {
+					player.sendMessage(ChatColor.RED + "Cannot set home location. You have too many set already.");
+					log.info(player.getName() + " tried to set home location [" + args[0] +"]. Too many set already.");
+				}
 			} else {
-				if (!this.Permissions.has(player, "multihome.home")) return true;
+				if (!this.Permissions.has(player, "multihome.sethome")) return true;
 				setPlayerHomeLocation(player, "", player.getLocation());
 				player.sendMessage(ChatColor.RED + "Home location set.");
 				log.info(player.getName() + " set home location [].");
